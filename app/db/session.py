45cfaea64581ffbd2f2abd07d0363616a78a055a
Nathan Schrader <natecschrader@gmail.com>
2024-07-22 10:31:32 -0700

    setup db conn, session management, base class for SQLAlchemy models

---
+++ app/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
+# used to set up the connection to the database
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# function is used to create new sessions that will be used to interact with the database.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# function is used to create a base class that your database models will inherit from, 
+# making it easy to define tables and columns in the database.
+Base = declarative_base()
+
