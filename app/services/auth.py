a27220fa452d828f825f2e5b34b1758097a8720a
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 11:28:58 -0700

    move return to the bottom of if statement

--- app/services/auth.py
+++ app/services/auth.py
@@ -29,14 +29,15 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
+
         if username is None or user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail='Could not validate user.')
+        return {'username': username, 'id': user_id}
+
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Could not validate credentials.'
         )
-
-    return {'username': username, 'id': user_id}
