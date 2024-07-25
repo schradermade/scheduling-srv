461150be9fbaec49c077112b70ed9271a099543a
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 11:54:57 -0700

    add fn to get current user

--- app/dependencies/dependencies.py
+++ app/dependencies/dependencies.py
@@ -1,5 +1,15 @@
 from services.auth import get_current_user
 from typing import Annotated
 from fastapi import Depends
+from fastapi import HTTPException
+from starlette import status
 
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+def get_current_user(user: user_dependency):
+  if user is None:
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail='Authentication Failed'
+    )
+  return user
