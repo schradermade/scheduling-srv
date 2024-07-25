17dce07524d7f80ec73db1aa278f28afaaaf4647
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 01:45:45 -0700

    auth endpoints

---
+++ app/routers/auth.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from models.user import Users
+from schemas.user import CreateUserRequest
+from dependencies.db import db_dependancy
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+from services.auth import authenticate_user
+from utils.security import bcrypt_context
+
+router = APIRouter()
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependancy,
+                      create_user_request: CreateUserRequest):
+    # create user model and assign the values from req body param
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+@router.post('/token')
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependancy):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
