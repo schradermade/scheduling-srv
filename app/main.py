d08cc65e4eab28aa2d6dc63e9c59ebf5623082f2
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 01:47:20 -0700

    fix imports; add prefix to router implementations

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from models.todo import Base
 from db.session import engine
-from endpoints import todo
+from routers.todo import router as TodoRouter
+from routers.auth import router as AuthRouter  # Ensure the import path is correct
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(todo.router)
+app.include_router(AuthRouter, prefix='/auth')
+app.include_router(TodoRouter, prefix='/todo')
