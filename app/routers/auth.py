4bdbc125a3d0d953e1ea78fbe0528ea1e7534932
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 02:06:21 -0700

    create token and return it; add response model of type Token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from typing import Annotated
 from fastapi import APIRouter, Depends
 from models.user import Users
@@ -7,6 +8,8 @@ from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
 from services.auth import authenticate_user
 from utils.security import bcrypt_context
+from services.auth import create_access_token
+from models.token import Token
 
 router = APIRouter()
 
@@ -27,10 +30,11 @@ async def create_user(db: db_dependancy,
     db.add(create_user_model)
     db.commit()
 
-@router.post('/token')
+@router.post('/token', response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependancy):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return 'Failed Authentication'
-    return 'Successful Authentication'
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return { 'access_token': token, 'token_type': 'bearer' }
