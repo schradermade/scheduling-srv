b134ebe63f90147ee0fd084e4a762005b2cb1b17
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 11:28:14 -0700

    fix dependency spelling error

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,24 +1,22 @@
 from datetime import timedelta
+from starlette import status
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
-from models.user import Users
-from schemas.user import CreateUserRequest
-from dependencies.db import db_dependancy
-from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
-from services.auth import authenticate_user
-from utils.security import bcrypt_context, oauth2_bearer
-from services.auth import create_access_token
+from models.user import Users
 from models.token import Token
+from schemas.user import CreateUserRequest
+from dependencies.db import db_dependency
+from services.auth import authenticate_user, create_access_token
+from utils.security import bcrypt_context
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
 
-
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def create_user(db: db_dependancy,
+async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
     # create user model and assign the values from req body param
     create_user_model = Users(
@@ -36,7 +34,7 @@ async def create_user(db: db_dependancy,
 
 @router.post('/token', response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-                                 db: db_dependancy):
+                                 db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
