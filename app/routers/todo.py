69f85197a1a698d6f1a777bd77127a483979aebb
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 11:55:24 -0700

    implement get_current_user fn into routes to check for logged in status

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path
 from models.todo import Todos
 from schemas.todo import TodoRequest
 from starlette import status
 from dependencies.db import db_dependency
-from dependencies.dependencies import user_dependency
+from dependencies.dependencies import get_current_user
 
 router = APIRouter(
   prefix='/todo',
@@ -11,23 +11,21 @@ router = APIRouter(
 )
 
 @router.get('/', status_code=status.HTTP_200_OK)
-async def read_all(user: user_dependency, db: db_dependency):
+async def read_all(db: db_dependency, user: dict = Depends(get_current_user)):
   return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
 
 @router.get('/{todo_id}', status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
-  todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0), user: dict = Depends(get_current_user)):
+  todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+    .filter(Todos.owner_id == user.get('id')).first()
   if todo_model is not None:
     return todo_model
   raise HTTPException(status_code=404, detail='Todo not found.')
 
 @router.post('', status_code=status.HTTP_201_CREATED)
-async def create_todo(user: user_dependency,
-                      db: db_dependency, 
-                      todo_request: TodoRequest):
-  if user is None:
-    raise HTTPException(status_code=401, detail='Authentication Failed')
-
+async def create_todo(db: db_dependency, 
+                      todo_request: TodoRequest, 
+                      user: dict = Depends(get_current_user)):
   todo_model = Todos(**todo_request.model_dump(), owner_id=user.get('id'))
   db.add(todo_model)
   db.commit()
@@ -35,8 +33,10 @@ async def create_todo(user: user_dependency,
 @router.put('/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db: db_dependency, 
                       todo_request: TodoRequest,
-                      todo_id: int = Path(gt=0)):
-  todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+                      todo_id: int = Path(gt=0),
+                      user: dict = Depends(get_current_user)):
+  todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+    .filter(Todos.owner_id == user.get('id')).first()
   if todo_model is None:
     raise HTTPException(status_code=404, detail='Todo not found')
   
