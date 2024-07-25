df85ad2bd7fa68580931ef9b96dd3fff2aa342d5
Nathan Schrader <natecschrader@gmail.com>
2024-07-25 09:51:02 -0700

    add fn to get current user with token

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,8 +1,11 @@
 from datetime import timedelta, datetime, timezone
+from typing import Annotated
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from models.user import Users
-from utils.security import bcrypt_context
-from jose import jwt
+from utils.security import bcrypt_context, oauth2_bearer
+from jose import jwt, JWTError
+from starlette import status
 
 SECRET_KEY = 'dc54fd359bd9ac6cc0cc262f4e6268e7f671ed988b447c72543f5d16c539de74'
 ALGORITHM = 'HS256'
@@ -20,3 +23,20 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({ 'exp': expires })
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate user.')
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate credentials.'
+        )
+
+    return {'username': username, 'id': user_id}
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,3 +1,5 @@
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
