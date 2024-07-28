291d955ba3aca560e11fefd4d64340e2d23e47a5
Nathan Schrader <natecschrader@gmail.com>
2024-07-28 16:43:24 -0700

    add put endpoint to change phone number

--- app/routers/users.py
+++ app/routers/users.py
@@ -28,3 +28,12 @@ async def change_password(db: db_dependency,
   user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
   db.add(user_model)
   db.commit()
+
+@router.put('/phonenumber/{phone_number}', status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(  phone_number: str, db:db_dependency, user: dict = Depends(get_current_user)):
+  if user is None:
+    raise HTTPException(status_code=401, deatil='Authentication Failed')
+  user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+  user_model.phone_number = phone_number
+  db.add(user_model)
+  db.commit()
