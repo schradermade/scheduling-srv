b56707796741f0edf0493d7913a30b9abf1baf8f
Nathan Schrader <natecschrader@gmail.com>
2024-07-28 16:44:26 -0700

    put db url in .env; change over to prod database

--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 # used to set up the connection to the database
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # function is used to create new sessions that will be used to interact with the database.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -13,4 +17,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # function is used to create a base class that your database models will inherit from, 
 # making it easy to define tables and columns in the database.
 Base = declarative_base()
-
