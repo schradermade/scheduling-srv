0b6f038dfbc4f5f8a2c789b1d81ef23045c4c8c1
Nathan Schrader <natecschrader@gmail.com>
2024-07-30 16:30:14 -0700

    add tests for todo endpoints: update, update not found, delete, delete not found

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -83,3 +83,41 @@ def test_read_one_authenticated_not_found():
   response = client.get('/todo/999')
   assert response.status_code == 404
   assert response.json() == { 'detail': 'Todo not found.' }
+  
+def test_update_todo(test_todo):
+  request_data={
+    'title': 'Change the title of the todo already saved!',
+    'description': 'Need to learn everyday!',
+    'priority': 5,
+    'complete': False
+  }
+  
+  response = client.put('/todo/1', json=request_data)
+  assert response.status_code == 204
+  db = TestingSessionLocal()
+  model = db.query(Todos).filter(Todos.id == 1).first()
+  assert model.title == 'Change the title of the todo already saved!'
+  
+def test_update_todo_not_found(test_todo):
+  request_data={
+    'title': 'Change the title of the todo already saved!',
+    'description': 'Need to learn everyday!',
+    'priority': 5,
+    'complete': False
+  }
+  
+  response = client.put('/todo/999', json=request_data)
+  assert response.status_code == 404
+  assert response.json() == { 'detail': 'Todo not found' }
+  
+def test_delete_todo(test_todo):
+  response = client.delete('/todo/1')
+  assert response.status_code == 204
+  db = TestingSessionLocal()
+  model = db.query(Todos).filter(Todos.id == 1).first()
+  assert model is None
+
+def test_delete_todo_not_found():
+  response = client.delete('/todo/999')
+  assert response.status_code == 404
+  assert response.json() == { 'detail': 'Todo not found' }
