3ac725da8f80a16ff837defd2f97af774f1988c0
Nathan Schrader <natecschrader@gmail.com>
2024-08-03 17:52:51 -0700

    add dependencies to user file

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,14 +1,21 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
+from pytest import Session
 from starlette import status
 from ..models import User, UserVerification
-from ..dependencies.db import db_dependency
 from ..dependencies.dependencies import get_current_user
+from ..dependencies.db import get_db
 from ..utils.security import bcrypt_context
+from passlib.context import CryptContext
 
 router = APIRouter(
   prefix='/user',
   tags=['user']
 )
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_user(db: db_dependency,
@@ -29,9 +36,9 @@ async def change_password(db: db_dependency,
   db.commit()
 
 @router.put('/phonenumber/{phone_number}', status_code=status.HTTP_204_NO_CONTENT)
-async def change_phone_number(  phone_number: str, db:db_dependency, user: dict = Depends(get_current_user)):
-  if user is None:
-    raise HTTPException(status_code=401, deatil='Authentication Failed')
+async def change_phone_number(phone_number: str, db:db_dependency, user: dict = Depends(get_current_user)):
+  # if user is None:
+  #   raise HTTPException(status_code=401, deatil='Authentication Failed')
   user_model = db.query(User).filter(User.id == user.get('id')).first()
   user_model.phone_number = phone_number
   db.add(user_model)
