96ed7232230f1ceb72c786b64ab6291aaf1586e2
Nathan Schrader <natecschrader@gmail.com>
2024-08-03 18:01:14 -0700

    fix broken endpoint test for auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,11 +5,13 @@ from app.dependencies.db import get_db
 app.dependency_overrides[get_db] = override_get_db
 
 def test_authenticate_user(test_user):
-  print("USER_OBJ:", test_user)
   db = TestingSessionLocal()
-  authenticated_user = authenticate_user(test_user.username, 'testpassword2', db)
-  assert authenticated_user is None
-  # assert authenticated_user.username == test_user.username
+  authenticated_user = authenticate_user(test_user.username, 'testpassword', db)
+  assert authenticated_user is not None
+  assert authenticated_user.username == test_user.username
   
-  # non_existent_user = authenticate_user('wrongusername', 'testpassword', db)
-  # assert non_existent_user is False
+  non_existent_user = authenticate_user('wrongusername', 'testpassword', db)
+  assert non_existent_user is False
+  
+  wrong_password_user = authenticate_user(test_user.username, 'wrongpassword', db)
+  assert wrong_password_user is False
