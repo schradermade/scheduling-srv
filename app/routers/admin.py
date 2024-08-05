8c6908e98d1f3d477fbf39466a2630d742b33374
Nathan Schrader <natecschrader@gmail.com>
2024-08-04 21:55:31 -0700

    add get_db fn into admin.py module

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,13 +1,28 @@
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, Depends
 from starlette import status
+from typing import Annotated
+from sqlalchemy.orm import Session
 from ..models import Todos
-from ..dependencies.db import db_dependency
-from ..dependencies.dependencies import user_dependency
+from ..dependencies.dependencies import get_current_user
+from ..db.session import SessionLocal
 
 router = APIRouter(
   prefix='/admin',
   tags=['admin']
 )
+
+def get_db():
+  db = SessionLocal()
+  try:
+    # yield means only the code prior to AND including the yield statement
+    # is executed before sending a response.
+    yield db
+    # this code after the yield is executed after the response has been delivered
+  finally:
+    db.close()
+    
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get('/todo', status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
