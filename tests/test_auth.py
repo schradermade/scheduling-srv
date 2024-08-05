b8c19b8f17268d5d088562686c563cc1ad1b20e0
Nathan Schrader <natecschrader@gmail.com>
2024-08-04 22:09:13 -0700

    add test for get_current_user

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,11 @@
 from .utils import *
 from app.services.auth import authenticate_user
 from app.dependencies.db import get_db
-from app.services.auth import create_access_token, SECRET_KEY, ALGORITHM
+from app.services.auth import create_access_token, get_current_user, SECRET_KEY, ALGORITHM
 from jose import jwt
 from datetime import timedelta
+import pytest
+from fastapi import HTTPException
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -32,4 +34,26 @@ def test_create_access_token():
   assert decoded_token['sub'] == username
   assert decoded_token['id'] == user_id
   assert decoded_token['role'] == role
+  
+@pytest.mark.asyncio
+async def test_get_current_user_valid():
+  encode = { 'sub': 'testuser', 'id': 1, 'role': 'admin'}
+  token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+  
+  user = await get_current_user(token=token)
+  
+  assert user == { 'username': 'testuser', 'id': 1, 'user_role': 'admin'}
+
+@pytest.mark.asyncio
+async def test_get_current_user_missing_payload():
+  encode = { 'role': 'user' }
+  token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+  
+  # If an exception raises during test
+  # alias it as excinfo
+  with pytest.raises(HTTPException) as excinfo:
+    await get_current_user(token=token)
+    
+  assert excinfo.value.status_code == 401
+  assert excinfo.value.detail == 'Could not validate user.'
   
