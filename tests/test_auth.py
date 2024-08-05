e312a80e40292ff964a4ea48942e96ce88357151
Nathan Schrader <natecschrader@gmail.com>
2024-08-04 21:56:09 -0700

    add test for create_access_token

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,9 @@
 from .utils import *
 from app.services.auth import authenticate_user
 from app.dependencies.db import get_db
+from app.services.auth import create_access_token, SECRET_KEY, ALGORITHM
+from jose import jwt
+from datetime import timedelta
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -15,3 +18,18 @@ def test_authenticate_user(test_user):
   
   wrong_password_user = authenticate_user(test_user.username, 'wrongpassword', db)
   assert wrong_password_user is False
+  
+def test_create_access_token():
+  username = 'testuser'
+  user_id = 1
+  role = 'user'
+  expires_delta = timedelta(days=1)
+  
+  token = create_access_token(username, user_id, role, expires_delta)
+  
+  decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options={'verify_signature': False})
+  
+  assert decoded_token['sub'] == username
+  assert decoded_token['id'] == user_id
+  assert decoded_token['role'] == role
+  
